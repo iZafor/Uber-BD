1193e1b6d122577ff1e151afb334584f2f1096df
RedwanGit <redwanrahmanstudent@gmail.com>
2023-11-30 17:39:03 +0600

    added back button support for maintenance scene

--- src/bd/uber/redwan/MaintainenceSceneController.java
+++ src/bd/uber/redwan/MaintainenceSceneController.java
@@ -4,16 +4,21 @@
  */
 package bd.uber.redwan;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -71,5 +76,16 @@ public class MaintainenceSceneController implements Initializable {
     @FXML
     private void viewReport(ActionEvent event) {
     }
+        
+    @FXML
+    private void backButton(ActionEvent event) throws IOException {
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainScene.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        stage.setScene(scene);
+        stage.show();        
+    }        
+    
     
 }
